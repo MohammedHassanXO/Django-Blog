bcbad34fac115fdd41a819b258ba7a24f0e2f7c5
Mohammed Hassan <mohammedhassan12_5@yahoo.com>
2023-10-11 01:18:00 +0300

    add post detail

--- Posts/views.py
+++ Posts/views.py
@@ -4,4 +4,9 @@ from .models import post
 
 def post_list(request):
     data = post.objects.all()
-    return render(request, 'posts.html', {'posts': data})
+    return render(request, 'posts.html', {'posts':data})
+
+def post_detail(request,post_id):
+    data = post.objects.get(id=post_id)
+    return render(request, 'post_detail.html', {'post':data})
+

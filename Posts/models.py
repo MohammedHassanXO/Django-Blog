64a9e5a03240a66d325edfbb72e96ec54f6d7a40
Mohammed Hassan <mohammedhassan12_5@yahoo.com>
2023-10-12 02:28:25 +0300

    add add-new post

--- Posts/models.py
+++ Posts/models.py
@@ -1,20 +1,16 @@
 from django.db import models
 from taggit.managers import TaggableManager
 from django.contrib.auth.models import User
+from django.utils import timezone
 # Create your models here.
 
-class post (models.Model):
+class post(models.Model):
     author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='post_author')
     title = models.CharField(max_length=100)
     content = models.TextField(max_length=50000)
-    publish_date = models.DateTimeField()
+    publish_date = models.DateTimeField(default=timezone.now)
     tags = TaggableManager()
     image = models.ImageField(upload_to='posts')
-    
-
-
-
-
     
     def __str__(self):
         return self.title 
--- Project/urls.py
+++ Project/urls.py
@@ -18,13 +18,15 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from Posts.views import post_list , post_detail
+from Posts.views import post_list, post_detail, add_post
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/',post_list),
     path('blog/<int:post_id>', post_detail),
+    path('blog/add', add_post ),
+
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
